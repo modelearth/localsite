949229056c75b006dbafd4c9ae042eb08039bc86
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2020-07-27 14:55:26 -0400

    fix for non bio 6 digit cat_filter

--- js/naics.js
+++ js/naics.js
@@ -441,7 +441,7 @@ function keyFound(this_key, cat_filter) {
         return true;
     } else if (params.go == "bioeconomy" && cat_filter.includes(this_key.slice(0,4))) { // Our 4 digit array matches key
         return true;
-    } else if (cat_filter.includes(this_key.slice(0,6))) {
+    } else if (params.go != "bioeconomy" && cat_filter.includes(this_key.slice(0,6))) {
         return true;
     } else {
         return false;
@@ -495,7 +495,11 @@ function topRatesInFips(dataSet, dataNames, fips, howMany, params){
                     if (cat_filter.length) {
                         cat_filt=[]
                         for(i=0;i<cat_filter.length;i++){
-                            cat_filt.push(cat_filter[i].slice(0,4))
+                            if(params.go == "bioeconomy"){
+                                cat_filt.push(cat_filter[i].slice(0,4))
+                            }else{
+                                cat_filt.push(cat_filter[i].slice(0,6))
+                            }
                         }
                         cat_filter=cat_filt
                         //console.log(cat_filter)
