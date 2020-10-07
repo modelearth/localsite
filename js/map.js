88c91fef2d056559b418674c8bbf481887887118
datascape <heijns@dreamstudio.com>
2020-10-07 14:34:14 -0400

    Distance lat/lon

--- js/map.js
+++ js/map.js
@@ -545,7 +545,10 @@ function addIcons(dp,map,map2) {
     } else if (element.website) {
       output += "<br>";
     }
-    
+    if (element[dp.latColumn]) {
+      //output += "distance: " + calculateDistance(element[dp.latColumn], element[dp.lonColumn], dp.latitude, dp.longitude, "M");
+    }
+
     // ADD POPUP BUBBLES TO MAP POINTS
     circle.bindPopup(output);
     circle2.bindPopup(output);
@@ -1671,7 +1674,22 @@ function lockSidemap() {
     mapFixed = false;
   }
 }
+function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+  var radlat1 = Math.PI * lat1/180
+  var radlat2 = Math.PI * lat2/180
+  //var radlon1 = Math.PI * lon1/180
+  //var radlon2 = Math.PI * lon2/180
+  var theta = lon1-lon2
+  var radtheta = Math.PI * theta/180
+  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  dist = Math.acos(dist)
+  dist = dist * 180/Math.PI
+  dist = dist * 60 * 1.1515
+  if (unit=="K") { dist = dist * 1.609344 } // Kilometers
+  if (unit=="N") { dist = dist * 0.8684 } // Nautical miles
+  return dist
+}
 $(window).resize(function() {
   $(".headerOffset2").height($("#filterFieldsHolder").height() + "px");
 });
-console.log('hello from dual map in localsite/js/map.js');
+console.log('end of localsite/js/map.js');
